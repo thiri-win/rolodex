a404fcfebc219bba8b9cb231a836520d1558fdb1
Thiri Win <thiriwinzhuang@gmail.com>
2020-03-06 20:07:02 +0630

    Updates

---
+++ precache-manifest.052d0dc7572a520da53c99965ec16419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d01700aa61e6c5409c0c03f4316a2e",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "54727b9897c5f77e594d",
+    "url": "/rolodex/static/css/main.d16462b7.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54727b9897c5f77e594d",
+    "url": "/rolodex/static/js/main.da9fc724.chunk.js"
+  },
+  {
+    "revision": "1818ea94fa687d54b234",
+    "url": "/rolodex/static/js/runtime-main.919454f4.js"
+  }
+]);
